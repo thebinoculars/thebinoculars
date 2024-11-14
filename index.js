5fa46729dc58d0229404143fc26dd76d5657d3f2
Hero <vndhero@gmail.com>
2024-11-14 21:04:37 +0700

    fix: update api

--- index.js
+++ index.js
@@ -1,59 +1,65 @@
 require('dotenv').config()
 const axios = require('axios')
 const moment = require('moment')
-const fs = require('fs');
+const fs = require('fs')
 
-(async () => {
-  try {
-    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/onecall', {
-      params: {
-        APPID: process.env.API_KEY,
-        lat: 21.027763,
-        lon: 105.83416,
-        lang: 'vi',
-        units: 'metric'
-      }
-    })
+const main = async () => {
+	try {
+		const params = {
+			appid: process.env.API_KEY,
+			lat: 21.027763,
+			lon: 105.83416,
+			lang: 'vi',
+			units: 'metric',
+		}
 
-    const icons = {
-      'Thunderstorm': '⚡',
-      'Drizzle': '🌧️',
-      'Rain': '⛈️',
-      'Snow': '❄️',
-      'Atmosphere': '🌫️',
-      'Clear': '☀️',
-      'Clouds': '☁️'
-    }
+		const [
+			{
+				data: { list },
+			},
+			{
+				data: { main, weather, wind, visibility },
+			},
+		] = await axios.all([
+			axios.get(`https://api.openweathermap.org/data/2.5/forecast`, { params }),
+			axios.get(`https://api.openweathermap.org/data/2.5/weather`, { params }),
+		])
 
-    const weatherTemplate = weather => weather.map(item => `${icons[item.main] || '🌦️'} ${item.description}`).join(', ')
+		const icons = {
+			Thunderstorm: '⚡',
+			Drizzle: '🌧️',
+			Rain: '⛈️',
+			Snow: '❄️',
+			Atmosphere: '🌫️',
+			Clear: '☀️',
+			Clouds: '☁️',
+		}
 
-    const UVStatus = index => {
-      if (index < 3) return 'thấp'
-      if (index < 6) return 'trung bình'
-      if (index < 8) return 'cao'
-      if (index < 11) return 'rất cao'
-      return 'độc hại'
-    }
+		const weatherTemplate = (weather) =>
+			weather
+				.map((item) => `${icons[item.main] || '🌦️'} ${item.description}`)
+				.join(', ')
 
-    const daily = data.daily.map(item => `| ${moment((item.dt + data.timezone_offset) * 1000).format('DD/MM')} | ${item.temp.min}\u2103 - ${item.temp.max}\u2103 | ${item.humidity}% | ${item.uvi} (${UVStatus(item.uvi)}) | ${weatherTemplate(item.weather)} |`).join('\n')
+		const hourly = list
+			.map(
+				(item) =>
+					`| ${moment(item.dt * 1000).format('DD/MM HH:mm')} | ${
+						item.main.temp_min
+					}\u2103 - ${item.main.temp_max}\u2103 | ${item.main.humidity}% | ${
+						item.visibility / 1000
+					}km | ${weatherTemplate(item.weather)} |`
+			)
+			.join('\n')
 
-    const hourly = data.hourly.map(item => `| ${moment((item.dt + data.timezone_offset) * 1000).format('DD/MM HH:mm')} | ${item.temp}\u2103 | ${item.humidity}% | ${item.visibility / 1000}km | ${weatherTemplate(item.weather)} |`).join('\n')
-
-    const template = `## 🌦️ Thời tiết Hà Nội (${moment().format('DD/MM')})
+		const template = `## 🌦️ Thời tiết Hà Nội (${moment().format('DD/MM')})
 
 ### Hiện tại
 
-- 🌡️ Nhiệt độ: ${data.current.temp}\u2103
-- 💦 Độ ẩm: ${data.current.humidity}%
-- 🌟 Chỉ số tia UV: ${data.current.uvi} (${UVStatus(data.current.uvi)})
-- 👁️ Tầm nhìn xa: ${data.current.visibility / 1000}km
-- ☂️ Thời tiết: ${weatherTemplate(data.current.weather)}
-
-### 7 ngày tới
-
-| Ngày | Nhiệt độ | Độ ẩm | Chỉ số tia UV | Thời tiết |
-| --- | --- | --- | --- | --- |
-${daily}
+- 🌡️ Nhiệt độ: ${main.temp}\u2103
+- 💦 Độ ẩm: ${main.humidity}%
+- 💨 Sức gió: ${wind.speed}m/s
+- 👁️ Tầm nhìn xa: ${visibility / 1000}km
+- ☂️ Thời tiết: ${weatherTemplate(weather)}
 
 ### Sắp tới
 
@@ -62,8 +68,10 @@ ${daily}
 ${hourly}
 `
 
-    fs.writeFileSync('README.md', template)
-  } catch (error) {
-    console.error(error)
-  }
-})()
+		fs.writeFileSync('README.md', template)
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+main()
